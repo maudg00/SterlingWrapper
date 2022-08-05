095253a846457d8a6c318e0fc71499628c5c476a
Mauricio <mauriciodg00@gmail.com>
2022-08-04 19:17:09 -0500

    .net framework compatibility and configuration

--- SterlingWrapper/Properties/AssemblyInfo.cs
+++ SterlingWrapper/Properties/AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("247441fb-74f9-4cd9-ac93-82c193ae82c2")]
